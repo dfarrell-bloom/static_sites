c2d46f467e0b1d754f87d66c283cd32a38f3116f
Dan Farrell <dfarrell@bloomhealthco.com>
2013-03-20 19:32:44 -0500

    pushing to test the pull later

---
+++ htdocs/index.php
@@ -0,0 +1,8 @@
+<?php 
+
+echo "PHP Works!  Wow, it feels strange writing php, which is weird considering I used to be a very LAMP-centric developer.  I guess I've grown"
+
+?>
+
+<p> Doc Root is: <code> <?php echo $_SERVER['DOCUMENT_ROOT'] ;?> </code> </p>
+<p>Http Host is: <code> <?php echo $_SERVER['DOCUMENT_ROOT'] ;?> </code></p>
